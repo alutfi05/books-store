eda458bcfb76224fa4127f20d16b5efc77d06ca6
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2022-08-21 15:37:10 +0700

    update progress books-store

--- server/app.js
+++ server/app.js
@@ -3,8 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const routes = require("./routes");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express"),
     swaggerDocument = require("./swagger.json");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
