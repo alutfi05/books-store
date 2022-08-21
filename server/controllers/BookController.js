449501beb2d797f2c990039ac3100730cf677382
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2022-08-21 15:26:58 +0700

    update progress books-store

--- server/controllers/BookController.js
+++ server/controllers/BookController.js
@@ -1,13 +1,38 @@
 class BookController {
-    static async getBooks(req, res) {}
+    static async getBooks(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    static async create(req, res) {}
+    static async create(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    static async detail(req, res) {}
+    static async detail(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    static async edit(req, res) {}
+    static async edit(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    static async delete(req, res) {}
+    static async delete(req, res) {
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 module.exports = BookController;
