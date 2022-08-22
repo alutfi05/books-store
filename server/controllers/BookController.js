e8c475faec678d202cb75e97ff0eb5ff019a1445
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2022-08-22 17:19:47 +0700

    make all function running in all controller

--- server/controllers/BookController.js
+++ server/controllers/BookController.js
@@ -1,36 +1,116 @@
+const { book, category, author, publisher } = require("../models");
+
 class BookController {
     static async getBooks(req, res) {
         try {
+            let books = await book.findAll({
+                order: [["id", "asc"]],
+                include: [category, author, publisher],
+            });
+
+            res.status(200).json(books);
         } catch (error) {
-            console.log(error);
+            res.status(500).json({ message: error.message });
         }
     }
 
     static async create(req, res) {
         try {
+            const {
+                title,
+                image,
+                synopsis,
+                price,
+                publicationYear,
+                categoryId,
+                authorId,
+                publisherId,
+            } = req.body;
+
+            let newBook = await book.create({
+                title,
+                image,
+                synopsis,
+                price,
+                publicationYear,
+                categoryId,
+                authorId,
+                publisherId,
+            });
+
+            res.status(201).json(newBook);
         } catch (error) {
-            console.log(error);
+            res.status(500).json({ message: error.message });
         }
     }
 
     static async detail(req, res) {
         try {
+            const id = +req.params.bookId;
+
+            let findBook = await book.findByPk(id, {
+                include: [category, author, publisher],
+            });
+
+            findBook !== null
+                ? res.status(200).json(findBook)
+                : res.status(404).json({ message: `Book id ${id} not found!` });
         } catch (error) {
-            console.log(error);
+            res.status(500).json({ message: error.message });
         }
     }
 
     static async edit(req, res) {
         try {
+            const id = +req.params.bookId;
+            const {
+                title,
+                image,
+                synopsis,
+                price,
+                publicationYear,
+                categoryId,
+                authorId,
+                publisherId,
+            } = req.body;
+
+            let updateBook = await book.update(
+                {
+                    title,
+                    image,
+                    synopsis,
+                    price,
+                    publicationYear,
+                    categoryId,
+                    authorId,
+                    publisherId,
+                },
+                { where: { id } }
+            );
+
+            updateBook[0] === 1
+                ? res
+                      .status(200)
+                      .json({ message: `Book id ${id} has been updated!` })
+                : res.status(404).json({ message: `Book id ${id} not found!` });
         } catch (error) {
-            console.log(error);
+            res.status(500).json({ message: error.message });
         }
     }
 
     static async delete(req, res) {
         try {
+            const id = +req.params.bookId;
+
+            let deleteBook = await book.destroy({ where: { id } });
+
+            deleteBook === 1
+                ? res
+                      .status(200)
+                      .json({ message: `Book id ${id} has been deleted!` })
+                : res.status(404).json({ message: `Book id ${id} not found!` });
         } catch (error) {
-            console.log(error);
+            res.status(500).json({ message: error.message });
         }
     }
 }
--- server/controllers/PublisherController.js
+++ server/controllers/PublisherController.js
@@ -1,13 +1,88 @@
+const { publisher, book } = require("../models");
+
 class PublisherController {
-    static async getPublishers(req, res) {}
+    static async getPublishers(req, res) {
+        try {
+            let publishers = await publisher.findAll({
+                order: [["id", "asc"]],
+                include: [book],
+            });
 
-    static async create(req, res) {}
+            res.status(200).json(publishers);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 
-    static async detail(req, res) {}
+    static async create(req, res) {
+        try {
+            const { name } = req.body;
 
-    static async edit(req, res) {}
+            let newPublisher = await publisher.create({ name });
 
-    static async delete(req, res) {}
+            res.status(201).json(newPublisher);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    static async detail(req, res) {
+        try {
+            const id = +req.params.publisherId;
+
+            let findPublisher = await publisher.findByPk(id, {
+                include: [book],
+            });
+
+            findPublisher !== null
+                ? res.status(200).json(findPublisher)
+                : res
+                      .status(404)
+                      .json({ message: `Publisher id ${id} not found!` });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    static async edit(req, res) {
+        try {
+            const id = +req.params.publisherId;
+            const { name } = req.body;
+
+            let updatePublisher = await publisher.update(
+                { name },
+                { where: { id } }
+            );
+
+            updatePublisher[0] === 1
+                ? res
+                      .status(200)
+                      .json({ message: `Publisher id ${id} has been updated!` })
+                : res
+                      .status(404)
+                      .json({ message: `Publisher id ${id} not found!` });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    static async delete(req, res) {
+        try {
+            const id = +req.params.publisherId;
+
+            let deletePublisher = await publisher.destroy({ where: { id } });
+
+            deletePublisher === 1
+                ? res
+                      .status(200)
+                      .json({ message: `Publisher id ${id} has been deleted!` })
+                : res
+                      .status(404)
+                      .json({ message: `Publisher id ${id} not found!` });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 }
 
 module.exports = PublisherController;
