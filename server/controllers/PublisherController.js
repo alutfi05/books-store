837bdc99053b858b3502d88037fa5bb5a442bd67
Muhammad Zikrullah <matakaca.mz30@gmail.com>
2022-08-22 11:16:06 +0700

    update controller and gitignore

--- server/controllers/PublisherController.js
+++ server/controllers/PublisherController.js
@@ -1,13 +1,53 @@
 class PublisherController {
-    static async getPublishers(req, res) {}
+    static async getPublishers(req, res) {
+        try{
 
-    static async create(req, res) {}
+        }catch(err){
+            res.status(500).json({
+                message: err.message
+            });
+        }
+    }
 
-    static async detail(req, res) {}
+    static async create(req, res) {
+        try{
 
-    static async edit(req, res) {}
+        }catch(err){
+            res.status(500).json({
+                message: err.message
+            });
+        }
+    }
 
-    static async delete(req, res) {}
+    static async detail(req, res) {
+        try{
+
+        }catch(err){
+            res.status(500).json({
+                message: err.message
+            });
+        }
+    }
+
+    static async edit(req, res) {
+        try{
+
+        }catch(err){
+            res.status(500).json({
+                message: err.message
+            });
+        }
+    }
+
+    static async delete(req, res) {
+        try{
+
+        }catch(err){
+            res.status(500).json({
+                message: err.message
+            });
+        }
+    }
 }
 
 module.exports = PublisherController;
