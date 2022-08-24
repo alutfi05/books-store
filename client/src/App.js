0c374032b44c1bf4cc9a5740b5a7072b01bab672
Muhammad Zikrullah <matakaca.mz30@gmail.com>
2022-08-24 11:12:09 +0700

    create client app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header'
+
+import SideBar from './components/sidebar';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <SideBar/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/mainContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BookCard from './bookCard';
+
+function MainContent() {
+    return ( 
+        <>
+        <div>
+            <BookCard/>
+        </div>
+        </>
+     );
+}
+
+export default MainContent;
